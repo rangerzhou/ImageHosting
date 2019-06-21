e4b62763f94207f781f1e7df9b0896f72c4e8ba6
ran.zhou <ran.zhou@aptiv.com>
2019-06-21 13:48:21 +0800

    Update

--- tools/Google_SS_Auto.sh
+++ tools/Google_SS_Auto.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
-#sudo apt update
-#sudo apt upgrade
-echo "********************** Open BBR **********************"
-echo "sudo -i"
-echo "echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf"
-echo "echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf"
-echo "sysctl -p"
+sudo apt -y update
+sudo apt -y upgrade
 
+echo "********************** Open BBR **********************"
 sudo sh -c "echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf"
 sudo sh -c "echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf"
 sudo sysctl -p
 sudo lsmod | grep bbr
 echo "******************* Open BBR Done ********************"
 
-#sudo pip install --upgrade pip
-#sudo pip install shadowsocks
+sudo apt -y install python-pip
+sudo pip install --upgrade pip
+sudo pip install shadowsocks
 sudo touch /etc/shadowsocks.json
 sudo sh -c "cat>/etc/shadowsocks.json<<EOF
 {
@@ -31,5 +28,6 @@ sudo /usr/local/bin/ssserver -c /etc/shadowsocks.json -d start
 echo "Add this line to /etc/rc.local for auto start:"
 echo "sudo /usr/local/bin/ssserver -c /etc/shadowsocks.json -d start"
 
-echo "VPC网络-防火墙规则-创建防火墙规则(来源IP地址范围：0.0.0.0/0，协议端口：tcp:12018)"
+echo "VPC网络-防火墙规则-创建防火墙规则(来源IP地址范围：0.0.0.0/0，协议端口：tcp:12888)"
 echo "VPC网络-外部IP地址-设置静态IP"
+
