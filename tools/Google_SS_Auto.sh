c37499530d575afe5c8fa8901e4e95b458042119
ran.zhou <ran.zhou@aptiv.com>
2019-06-24 10:22:08 +0800

    Update

--- tools/Google_SS_Auto.sh
+++ tools/Google_SS_Auto.sh
@@ -24,10 +24,12 @@ sudo sh -c "cat>/etc/shadowsocks.json<<EOF
   "\"method"\":"\"aes-256-cfb"\"
 }
 EOF"
+echo "修改 server_port 和 password 请修改文件：/etc/shadowsocks.json"
 sudo /usr/local/bin/ssserver -c /etc/shadowsocks.json -d start
-echo "Add this line to /etc/rc.local for auto start:"
+echo "添加下面这行到 /etc/rc.local 以自动重启 SS ......"
 echo "sudo /usr/local/bin/ssserver -c /etc/shadowsocks.json -d start"
 
-echo "VPC网络-防火墙规则-创建防火墙规则(来源IP地址范围：0.0.0.0/0，协议端口：tcp:18888)"
-echo "VPC网络-外部IP地址-设置静态IP"
-
+echo "在 Google cloud 控制台中："
+echo "    1. VPC网络-防火墙规则-创建防火墙规则(来源IP地址范围：0.0.0.0/0，协议端口：tcp:18888)"
+echo "    2. VPC网络-外部IP地址-设置静态IP"
+echo "********************************** 配置完成 **********************************"
