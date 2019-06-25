6cdbf54acb753e3fa63080417931402eb8da7c63
ran.zhou <ran.zhou@aptiv.com>
2019-06-25 10:12:10 +0800

    Updated for configuration auto start SS to /etc/rc.local

--- tools/Google_SS_Auto.sh
+++ tools/Google_SS_Auto.sh
@@ -24,10 +24,13 @@ sudo sh -c "cat>/etc/shadowsocks.json<<EOF
   "\"method"\":"\"aes-256-cfb"\"
 }
 EOF"
-echo "修改 server_port 和 password 请修改文件：/etc/shadowsocks.json"
+echo "######## Change "server_port" and "password" ：/etc/shadowsocks.json ########"
 sudo /usr/local/bin/ssserver -c /etc/shadowsocks.json -d start
-echo "添加下面这行到 /etc/rc.local 以自动重启 SS ......"
-echo "sudo /usr/local/bin/ssserver -c /etc/shadowsocks.json -d start"
+echo "Configuration auto start SS ......"
+sudo sh -c "sed '/exit/i \sudo /usr/local/bin/ssserver -c /etc/shadowsocks.json -d start' /etc/shadowsocks.json > /etc/rc.local.bak"
+sudo mv /etc/rc.local.bak /etc/rc.local
+sudo chmod 755 /etc/rc.local
+echo "SS auto start configuration complete......"
 
 echo "在 Google cloud 控制台中："
 echo "    1. VPC网络-防火墙规则-创建防火墙规则(来源IP地址范围：0.0.0.0/0，协议端口：tcp:18888)"
